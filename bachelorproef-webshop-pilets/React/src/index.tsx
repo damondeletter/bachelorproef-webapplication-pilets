2acd04416517814fb3aa99446fcc9ccc979f20f9
damondeletter <damon.deletter@student.hogent.be>
2023-04-01 15:16:32 +0200

    Update

--- bachelorproef-webshop-pilets/React/src/index.tsx
+++ bachelorproef-webshop-pilets/React/src/index.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 import type { PiletApi } from 'bachelorproef-app-shell';
 import { ProductPage } from './ProductPage';
 import products from './mock/products';
-
-const Page = React.lazy(() => import('./Page'));
 
 export function setup(app: PiletApi) {
   
